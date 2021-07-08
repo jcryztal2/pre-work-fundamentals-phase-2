c9d6cddbfc5d3f40ced48a174f5bc09905a76a39
Crystal J <cjohn12@cnm.edu>
2021-07-07 20:54:01 -0600

    created and added conditionals, functions, variables, data, object, and apis ,js files

---
+++ objects-apis.js
@@ -0,0 +1,31 @@
+/*
+ *Objects, Interfaces, and API's
+ */
+
+let dog = {
+    name: " Steve",
+    color: "brown",
+    breed: "Pug",
+    size: "Small",
+    bark: function(typeOfBark){
+       console.log("Bark!")
+    }
+}
+
+dog.bark();
+
+// API = Application Program Interface
+/*...*/
+//Passing By Value & Passing By Reference
+
+function x(y) {
+    y.num = y.num + 5;
+    console.log(y);
+}
+
+let y = {
+    name: "Tom",
+    num: 10,
+};
+x(y);
+console.log(y);
